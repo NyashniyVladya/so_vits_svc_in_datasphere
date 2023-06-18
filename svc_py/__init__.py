1c19a9ddc9202753d9c980e1c0bc1f6d8d365c2c
Vladya <19994753+NyashniyVladya@users.noreply.github.com>
2023-06-18 16:26:02 +0300

    v1.0.9

--- svc_py/__init__.py
+++ svc_py/__init__.py
@@ -3,6 +3,6 @@
 @author: Vladya
 """
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 
 from .main import convert_folder
--- svc_py/main.py
+++ svc_py/main.py
@@ -15,14 +15,14 @@ class InseparableFile(Exception):
     pass
 
 
-def convert_folder(folder, out_folder, debug=False, ff_log_level=None):
+def convert_folder(folder, out_folder, cmd, debug=False, ff_log_level=None):
     folder, out_folder = map(path.abspath, (folder, out_folder))
     for audio_file in map(path.abspath, librosa.util.find_files(folder)):
         _out, _ = path.splitext(path.relpath(audio_file, folder))
         _out = "{0}.wav".format(_out.replace(os.sep, '_'))
         out_path = path.abspath(path.join(out_folder, _out))
         try:
-            prepare_audio_to_generate(audio_file, out_path, ff_log_level)
+            prepare_audio_to_generate(audio_file, out_path, cmd, ff_log_level)
         except InseparableFile as ex:
             if debug:
                 raise ex
@@ -84,7 +84,7 @@ def crop_audio(y_array, sr, max_duration=9.):
             return result
 
 
-def prepare_audio_to_generate(_from, _to, ff_log_level=None):
+def prepare_audio_to_generate(_from, _to, cmd, ff_log_level=None):
 
     _from, _to = map(path.abspath, (_from, _to))
     out_name, out_ext = path.splitext(_to)
@@ -112,6 +112,8 @@ def prepare_audio_to_generate(_from, _to, ff_log_level=None):
             if ff_log_level:
                 kw["ff_log_level"] = ff_log_level
 
+            kw["cmd"] = cmd
+
             name_now = convert(
                 name_now,
                 _get_tmp_name(out_ext, _tmp_dir),
@@ -135,6 +137,7 @@ def prepare_audio_to_generate(_from, _to, ff_log_level=None):
 def convert(_from, _to, **kwargs):
 
     ff_log_level = kwargs.pop("ff_log_level", "error")
+    cmd = kwargs.pop("cmd", "ffmpeg")
 
     _from, _to = map(path.abspath, (_from, _to))
 
@@ -146,6 +149,6 @@ def convert(_from, _to, **kwargs):
     stream = stream.global_args("-loglevel", ff_log_level)
 
     _create_dir_for_file(_to)
-    stream.run(overwrite_output=True)
+    stream.run(overwrite_output=True, cmd=cmd)
 
     return _to
